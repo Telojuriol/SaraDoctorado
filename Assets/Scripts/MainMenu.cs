83afe3afa3234dc02482afb019f61102897e770e
Telojuri <uribcn1994@gmail.com>
2025-03-04 09:35:33 +0100

    FEATURE: Removed everything

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class MainMenu : MonoBehaviour
 {
     [Tooltip("El botón que inicia el juego.")]
     public GameObject startButtonObject; // Usar GameObject para el botón
-
-    private XRSimpleInteractable startButton;
 
     void Start()
     {
@@ -21,11 +17,7 @@ public class MainMenu : MonoBehaviour
     {
         if (startButtonObject != null)
         {
-            startButton = startButtonObject.GetComponent<XRSimpleInteractable>();
-            if (startButton == null)
-            {
-                Debug.LogError("No se pudo encontrar el componente XRSimpleInteractable en el objeto del botón.");
-            }
+
         }
         else
         {
@@ -36,17 +28,11 @@ public class MainMenu : MonoBehaviour
     // Añade el listener al botón de inicio
     void AddStartButtonListener()
     {
-        if (startButton != null)
-        {
-            startButton.selectEntered.AddListener(OnStartButtonSelected);
-        }
+
     }
 
     // Evento que se ejecuta al seleccionar el botón de inicio
-    private void OnStartButtonSelected(SelectEnterEventArgs args)
-    {
-        LoadGameScene();
-    }
+
 
     // Carga la escena del juego
     void LoadGameScene()
